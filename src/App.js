da573b7c64bd43d5b1868857fb4a492447147815
BrennanKnopf <brennanknopf@gmail.com>
2022-02-24 13:28:55 -0600

    Created navigationBar component. Connected Navigation Bar to app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import { Routes, Route } from 'react-router-dom'
-import {useEffect, useState} from 'react'
-import LoginForm from './Components/LoginForm/LoginForm'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { Routes, Route } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import LoginForm from './Components/LoginForm/LoginForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavigationBar from './Components/NavigationBar/NavigationBar';
+import jwt_decode from 'jwt-decode';
 
 function App() {
   
-  // const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null)
 
-  // useEffect(() => {
+  useEffect(() => {
 
-  //   const jwt = localStorage.getItem('token');
-  //   try {
-  //     const decodedUser = jwt_decode(jwt);
-  //     setUser(decodedUser);
+    const jwt = localStorage.getItem('token');
+    try {
+      const decodedUser = jwt_decode(jwt);
+      setUser(decodedUser);
 
-  //   } catch { }
-  // }, [])
+    } catch { }
+  }, [])
 
 
   return (
-    <div className="App">
-      {/* <Nav/> */}
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-      </Routes>
+    <div>
+      <NavigationBar user={user} />
+      <div>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+        </Routes>
+      </div>
     </div>
   );
 }
