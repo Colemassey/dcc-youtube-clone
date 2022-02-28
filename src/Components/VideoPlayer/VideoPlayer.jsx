22a862c72d8d7a62dd804cbb77708100c4f8279a
BrennanKnopf <brennanknopf@gmail.com>
2022-02-28 13:20:45 -0600

    A

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import YoutubeEmbed from "./YoutubeEmbed";
-import HomePage from "../HomePage/HomePage";
 
 function VideoPlayer() {
   return (
     <div className="App">
       <h1>Youtube Embed</h1>
-      <YoutubeEmbed embedId={searchVideoID} />
+      <YoutubeEmbed embedId={setSearchVideoID} />
     </div>
   );
 }
