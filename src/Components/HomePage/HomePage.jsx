64844d45369c99b8026442948b1039afceb102bf
BrennanKnopf <brennanknopf@gmail.com>
2022-02-28 16:12:10 -0600

    still working on connecting the currently played video to the related videos.

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -2,22 +2,37 @@ import React, {useState} from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import VideoPlayer from '../VideoPlayer/VideoPlayer';
 import axios from 'axios';  
+import RelatedVideos from '../RelatedVideos/RelatedVideos';
 
 const HomePage = (props) => {
     
     const [searchVideoID, setSearchVideoID] = useState("")
+    const [relatedVideo, setRelatedVideo] = useState([])
     
+
     async function searchYouTube(searchTerm) {
         let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyCA_6OGhIBDkYUzdjQOPRD535rx3l8GyP8&type=video&q=${searchTerm}`);
-        console.log(response.data)
+        // console.log(response.data)
         setSearchVideoID(response.data.items[0].id.videoId)
     }
     
-    return ( 
+
+    async function getRelatedVideos (searchVideoID) {
+        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${searchVideoID}&type=video&key=AIzaSyCA_6OGhIBDkYUzdjQOPRD535rx3l8GyP8`);
+        console.log(response)
+        let relatedVideos = response.data.items.filter(video => video.snippet)
+        setRelatedVideo(relatedVideos)
+    }
+        
+   
+   
+   
+        return ( 
 
         <div>
-            <SearchBar searchYouTube={searchYouTube} />
+            <SearchBar searchYouTube={searchYouTube}  />
             <VideoPlayer searchVideoID={searchVideoID} />
+            {/* <RelatedVideos relatedVideos={relatedVideo} getRelatedVideos={getRelatedVideos} /> */}
         </div>
      );
 }
---
+++ src/Components/RelatedVideos/RelatedVideos.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const RelatedVideos = (props) => {
+    console.log(props.relatedVideo)        
+    function videoLinks() {
+        return props.relatedVideo.map((video) => {
+            return (
+                    <li><a href={`http://www.youtube.com/watch?v=${video.videoId}`}>{video.videoTitle}</a> </li>
+                    );
+        });
+    }
+        return (
+            <div>
+                {videoLinks()}
+            </div>
+        );
+}
+ 
+export default RelatedVideos;
