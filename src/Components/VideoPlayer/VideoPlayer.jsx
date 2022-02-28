4bdeead53af688b280f316cc9236a69c240f0870
TylerHardiman <Fulldadmode89@gmail.com>
2022-02-28 14:17:19 -0500

    Designed the outline for our card to embed youtube and input required information.

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { Card } from 'react-bootstrap';
+import {ListGroup, ListGroupItem} from 'react-bootstrap';
 import YoutubeEmbed from "./YoutubeEmbed";
 
 function VideoPlayer() {
   return (
     <div className="App">
-      <h1>Youtube Embed</h1>
-      <YoutubeEmbed embedId="rokGy0huYEA" />
+   <Card style={{ width: '72rem' }}>
+   <Card.Header>Youtube Playa</Card.Header>
+<Card.Title>Press the button Playa</Card.Title>
+<YoutubeEmbed embedId="rokGy0huYEA" />
+  <Card.Body>
+    <Card.Title>Video Title goes here</Card.Title>
+    <Card.Text>
+      Youtube video description goes here
+    </Card.Text>
+  </Card.Body>
+  <ListGroup className="list-group-flush">
+    <ListGroupItem>Video Reccomendation goes here</ListGroupItem>
+    <ListGroupItem>Video Reccomendation goes here</ListGroupItem>
+    <ListGroupItem>Video Reccomendation goes here</ListGroupItem>
+  </ListGroup>
+  <Card.Body>
+    <Card.Link href="#">Links if we want</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
     </div>
   );
 }
