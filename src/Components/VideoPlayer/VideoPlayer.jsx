8886795ee8f319a95bbf9b032a9fdf4572b20f2e
BrennanKnopf <brennanknopf@gmail.com>
2022-03-01 08:21:46 -0600

    Restored searhbar and video player connection for testing.

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { Card } from 'react-bootstrap';
-import {ListGroup, ListGroupItem} from 'react-bootstrap';
 import YoutubeEmbed from "./YoutubeEmbed";
 
 
 
-function VideoPlayer() {
+function VideoPlayer(props) {
   return (
     <div className="App">
    <Card style={{ width: '72rem' }}>
    <Card.Header>Youtube Playa</Card.Header>
 <Card.Title>Press the button Playa</Card.Title>
-<YoutubeEmbed embedId={'81i78S1eBFs'} />
+<YoutubeEmbed embedId={props.searchVideoID} />
   <Card.Body>
     <Card.Title>Video Title goes here</Card.Title>
     <Card.Text>
