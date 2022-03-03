5c3dc4973acd36c1f83c8d8c7ff1a1ed2f05cdae
Cole Massey <cole.massey33@gmail.com>
2022-03-02 21:48:48 -0600

    started styling homepage

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import VideoPlayer from '../VideoPlayer/VideoPlayer';
-import axios from 'axios';  
+import axios from 'axios';
 import RelatedVideos from '../RelatedVideos/RelatedVideos';
 
 
@@ -11,9 +11,9 @@ const HomePage = (props) => {
     const [searchVideoID, setSearchVideoID] = useState("dQw4w9WgXcQ")
     const [videoDescription, setVideoDescription] = useState('The official video for “Never Gonna Give You Up” by Rick Astley')
     // console.log(videoDescription)
-   
-    
-    
+
+
+
 
     async function searchYouTube(searchTerm) {
         let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyAHwyZPFnq_dbkrFRGOW98ZqSCXx0VrGxU&part=snippet&type=video&q=${searchTerm}`);
@@ -21,23 +21,23 @@ const HomePage = (props) => {
         setSearchVideoID(response.data.items[0].id.videoId)
         setVideoTitle(response.data.items[0].snippet.title)
         setVideoDescription(response.data.items[0].snippet.description)
-        
+
     }
-            
-    return ( 
+
+    return (
 
         <div>
             <SearchBar searchYouTube={searchYouTube} />
             <div className='parent'>
                 <div className='div1'>
-                    <VideoPlayer searchVideoID={searchVideoID} videoTitle={videoTitle} videoDescription={videoDescription} />
+                    <RelatedVideos searchVideoID={searchVideoID} setSearchVideoID={setSearchVideoID} setVideoTitle={setVideoTitle} setVideoDescription={setVideoDescription} />
                 </div>
                 <div className='div2'>
-                    <RelatedVideos searchVideoID={searchVideoID} setSearchVideoID={setSearchVideoID} setVideoTitle={setVideoTitle} setVideoDescription={setVideoDescription} />
+                    <VideoPlayer searchVideoID={searchVideoID} videoTitle={videoTitle} videoDescription={videoDescription} />
                 </div>
             </div>
         </div>
-        )
+    )
 
 }
 export default HomePage;
